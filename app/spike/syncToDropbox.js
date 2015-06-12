f10a2dad7957be73968e12d77757521515d1a92c
tompascall <tompascall@gmail.com>
2015-06-12 14:10:24 +0200

    refactor spike

--- app/spike/syncToDropbox.js
+++ app/spike/syncToDropbox.js
@@ -4,46 +4,62 @@
 
 // Browser-side applications do not use the API secret.
 var client = new Dropbox.Client({ key: 'pbqyznysf6jffyl' });
-
-var showError = function (error) {
-  switch (error.status) {
-  case Dropbox.ApiError.INVALID_TOKEN:
+var eTokens = {
+  INVALID_TOKEN: Dropbox.ApiError.INVALID_TOKEN,
     // If you're using dropbox.js, the only cause behind this error is that
     // the user token expired.
     // Get the user through the authentication flow again.
-    break;
-
-  case Dropbox.ApiError.NOT_FOUND:
+  NOT_FOUND: Dropbox.ApiError.NOT_FOUND,
     // The file or folder you tried to access is not in the user's Dropbox.
     // Handling this error is specific to your application.
-    break;
-
-  case Dropbox.ApiError.OVER_QUOTA:
+  OVER_QUOTA: Dropbox.ApiError.OVER_QUOTA,
     // The user is over their Dropbox quota.
     // Tell them their Dropbox is full. Refreshing the page won't help.
-    break;
-
-  case Dropbox.ApiError.RATE_LIMITED:
+  RATE_LIMITED: Dropbox.ApiError.RATE_LIMITED,
     // Too many API requests. Tell the user to try again later.
     // Long-term, optimize your code to use fewer API calls.
-    break;
-
-  case Dropbox.ApiError.NETWORK_ERROR:
+  NETWORK_ERROR: Dropbox.ApiError.NETWORK_ERROR,
     // An error occurred at the XMLHttpRequest layer.
     // Most likely, the user's network connection is down.
     // API calls will not succeed until the user gets back online.
-    break;
-
-  case Dropbox.ApiError.INVALID_PARAM:
-  case Dropbox.ApiError.OAUTH_ERROR:
-  case Dropbox.ApiError.INVALID_METHOD:
-  default:
-    // Caused by a bug in dropbox.js, in your application, or in Dropbox.
-    // Tell the user an error occurred, ask them to refresh the page.
-  }
-  console.log(error.status);
+  INVALID_PARAM: Dropbox.ApiError.INVALID_PARAM,
+  OAUTH_ERROR: Dropbox.ApiError.OAUTH_ERROR,
+  INVALID_METHOD: Dropbox.ApiError.INVALID_METHOD
 };
 
+var showError = {};
+
+function createShowError (message) {
+  var message = message || 'Something went wrong, error status code: ';
+    return function (error) {
+    console.log(message, error.status);
+  };
+}
+
+function fillShowError () {
+  for (var token in eTokens) {
+    showError[token] = createShowError(token);
+  }
+}
+
+function showError (error) {
+  if (showError[error.status]) {
+    showError[error.status](error);
+  }
+
+}
+
+function doSomethingCool () {
+
+  client.getAccountInfo(function(error, accountInfo) {
+    if (error) {
+      return  showError[error.status](error);  // Something went wrong.
+    }
+    window.alert('Hello, ' + accountInfo.name + '!');
+  });
+}
+
+fillShowError();
 client.authenticate(function(error, client) {
   if (error) {
     // Replace with a call to your own error-handling code.
@@ -51,7 +67,7 @@ client.authenticate(function(error, client) {
     // Don't forget to return from the callback, so you don't execute the code
     // that assumes everything went well.
     console.log('authentication error');
-    return showError(error);
+    return  showError[error.status](error);
   }
 
   // Replace with a call to your own application code.
@@ -63,15 +79,7 @@ client.authenticate(function(error, client) {
   doSomethingCool(client);
 });
 
-function doSomethingCool () {
 
-  client.getAccountInfo(function(error, accountInfo) {
-    if (error) {
-      return showError(error);  // Something went wrong.
-    }
-    alert("Hello, " + accountInfo.name + "!");
-  });
-}
 
 
 
