eb802db8ae9dcc7b9940080c26ba6193c127f734
tompascall <tompascall@gmail.com>
2015-06-12 22:11:20 +0200

    dropbox spike has been refactored

--- app/spike/syncToDropbox.js
+++ app/spike/syncToDropbox.js
@@ -4,62 +4,103 @@
 
 // Browser-side applications do not use the API secret.
 var client = new Dropbox.Client({ key: 'pbqyznysf6jffyl' });
-var eTokens = {
-  INVALID_TOKEN: Dropbox.ApiError.INVALID_TOKEN,
+
+var dropErrorHandlers = {
+  showTokenNumber: function (tokenNumber, message) {
+    var message = message || 'Something went wrong: ';
+    console.log(message, tokenNumber);
+  }
+};
+
+dropErrorHandlers[Dropbox.ApiError.INVALID_TOKEN] = {
     // If you're using dropbox.js, the only cause behind this error is that
     // the user token expired.
     // Get the user through the authentication flow again.
-  NOT_FOUND: Dropbox.ApiError.NOT_FOUND,
+  tokenNumber: Dropbox.ApiError.INVALID_TOKEN,
+  errorHandler: function () {
+    var message = 'The user token expired.';
+    dropErrorHandlers.showTokenNumber(this.tokenNumber, message);
+  }
+};
+
+dropErrorHandlers[Dropbox.ApiError.NOT_FOUND] = {
     // The file or folder you tried to access is not in the user's Dropbox.
     // Handling this error is specific to your application.
-  OVER_QUOTA: Dropbox.ApiError.OVER_QUOTA,
+  tokenNumber: Dropbox.ApiError.NOT_FOUND,
+  errorHandler: function () {
+    var message = 'The file or folder you tried to access is not in the user\'s Dropbox';
+    dropErrorHandlers.showTokenNumber(this.tokenNumber, message);
+  }
+};
+
+dropErrorHandlers[Dropbox.ApiError.OVER_QUOTA] = {
     // The user is over their Dropbox quota.
     // Tell them their Dropbox is full. Refreshing the page won't help.
-  RATE_LIMITED: Dropbox.ApiError.RATE_LIMITED,
+  tokenNumber: Dropbox.ApiError.OVER_QUOTA,
+  errorHandler: function () {
+    var message = 'The user is over their Dropbox quota';
+    dropErrorHandlers.showTokenNumber(this.tokenNumber, message);
+  }
+};
+
+dropErrorHandlers[Dropbox.ApiError.RATE_LIMITED] = {
     // Too many API requests. Tell the user to try again later.
     // Long-term, optimize your code to use fewer API calls.
-  NETWORK_ERROR: Dropbox.ApiError.NETWORK_ERROR,
+  tokenNumber: Dropbox.ApiError.RATE_LIMITED,
+  errorHandler: function () {
+    var message = 'Too many API requests, try again later';
+    dropErrorHandlers.showTokenNumber(this.tokenNumber, message);
+  }
+};
+
+dropErrorHandlers[Dropbox.ApiError.NETWORK_ERROR] = {
     // An error occurred at the XMLHttpRequest layer.
     // Most likely, the user's network connection is down.
     // API calls will not succeed until the user gets back online.
-  INVALID_PARAM: Dropbox.ApiError.INVALID_PARAM,
-  OAUTH_ERROR: Dropbox.ApiError.OAUTH_ERROR,
-  INVALID_METHOD: Dropbox.ApiError.INVALID_METHOD
+  tokenNumber: Dropbox.ApiError.NETWORK_ERROR,
+  errorHandler: function () {
+    var message = 'An error occurred at the XMLHttpRequest layer';
+    dropErrorHandlers.showTokenNumber(this.tokenNumber, message);
+  }
 };
 
-var showError = {};
-
-function createShowError (message) {
-  var message = message || 'Something went wrong, error status code: ';
-    return function (error) {
-    console.log(message, error.status);
-  };
-}
-
-function fillShowError () {
-  for (var token in eTokens) {
-    showError[token] = createShowError(token);
+dropErrorHandlers[Dropbox.ApiError.INVALID_PARAM] = {
+  tokenNumber: Dropbox.ApiError.INVALID_PARAM,
+  errorHandler: function () {
+    var message = 'Invalid parameter';
+    dropErrorHandlers.showTokenNumber(this.tokenNumber, message);
   }
-}
+};
 
-function showError (error) {
-  if (showError[error.status]) {
-    showError[error.status](error);
+dropErrorHandlers[Dropbox.ApiError.OAUTH_ERROR] = {
+  tokenNumber: Dropbox.ApiError.OAUTH_ERROR,
+  errorHandler: function () {
+    var message = 'Authentication error';
+    dropErrorHandlers.showTokenNumber(this.tokenNumber, message);
   }
+};
 
-}
+dropErrorHandlers[Dropbox.ApiError.INVALID_METHOD] = {
+  tokenNumber: Dropbox.ApiError.INVALID_METHOD,
+  errorHandler: function () {
+    var message = 'Invalid method';
+    dropErrorHandlers.showTokenNumber(this.tokenNumber, message);
+  }
+};
 
 function doSomethingCool () {
 
   client.getAccountInfo(function(error, accountInfo) {
     if (error) {
-      return  showError[error.status](error);  // Something went wrong.
+      return  dropErrorHandlers[error.status].errorHandler();  // Something went wrong.
     }
-    window.alert('Hello, ' + accountInfo.name + '!');
+    else {
+      window.alert('Hello, ' + accountInfo.name + '!');
+      client.signOut();
+    }
   });
 }
 
-fillShowError();
 client.authenticate(function(error, client) {
   if (error) {
     // Replace with a call to your own error-handling code.
@@ -67,7 +108,7 @@ client.authenticate(function(error, client) {
     // Don't forget to return from the callback, so you don't execute the code
     // that assumes everything went well.
     console.log('authentication error');
-    return  showError[error.status](error);
+    return  dropErrorHandlers[error.status].errorHandler();
   }
 
   // Replace with a call to your own application code.
@@ -77,6 +118,7 @@ client.authenticate(function(error, client) {
 
   console.log('client is ready');
   doSomethingCool(client);
+
 });
 
 
